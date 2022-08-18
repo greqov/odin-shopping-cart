f2feed870efe40b59529a0f412c99619aaf8e6a9
Ivan Greqov <96950329+greqov@users.noreply.github.com>
2022-08-18 12:05:57 +0300

    Initial commit

---
+++ src/js/__tests__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App component', () => {
+  it('renders correct heading', () => {
+    render(<App />);
+    expect(screen.getByRole('heading')).toHaveTextContent(/such app!/i);
+  });
+});
