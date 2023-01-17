9520d748a3bd7de4ef7f1d343635473dd779ac35
Ivan Greqov <idgreq@bk.ru>
2023-01-17 12:29:59 +0300

    App: add basic router test

--- src/js/__tests__/App.test.js
+++ src/js/__tests__/App.test.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('App component', () => {
-  it('renders correct heading', () => {
+  it('renders correctly', () => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     );
+
     expect(screen.getByRole('heading')).toHaveTextContent(/such app/i);
+  });
+
+  // TODO: update this basic test to make sure router is okayish
+  test('click on "shop" link navigate to Shop page', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    await userEvent.click(screen.getByText(/shop/i));
+
+    expect(screen.getByRole('heading')).toHaveTextContent(/such shop/i);
   });
 });
