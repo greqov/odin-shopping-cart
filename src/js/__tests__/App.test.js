18e1f3cd7ad7f08fdd3a18713efe9e8538d58cdb
Ivan Greqov <idgreq@bk.ru>
2023-01-17 13:03:52 +0300

    App: update tests (replace title, add render wrapper)

--- src/js/__tests__/App.test.js
+++ src/js/__tests__/App.test.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 // TODO: add before each setup?
 
 describe('App component', () => {
   it('renders correctly', () => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    );
+    render(<App />, { wrapper: BrowserRouter });
 
     expect(screen.getByRole('heading')).toHaveTextContent(/such app/i);
   });
 
   // TODO: update this basic test to make sure router is okayish
-  test('click on "shop" link navigate to Shop page', async () => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    );
+  test('shows Shop page after clicking on "shop" link', async () => {
+    render(<App />, { wrapper: BrowserRouter });
 
     await userEvent.click(screen.getByText(/shop/i));
 
