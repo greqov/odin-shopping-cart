6f729ba776a0fd05277b09a17752ca70921d12df
Ivan Greqov <idgreq@bk.ru>
2023-03-17 11:19:42 +0300

    Add cart icon component

---
+++ src/js/__tests__/CartIcon.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CardIcon from '../components/CartIcon';
+
+describe('Cart icon', () => {
+  test('renders correctly', () => {
+    render(<CardIcon />);
+    // TODO: don't like getByTitle here
+    expect(screen.getByTitle(/go to cart page/i)).toBeInTheDocument();
+  });
+
+  test('shows total price', () => {
+    render(<CardIcon price="12" />);
+    expect(screen.getByText(/\$12/i)).toBeInTheDocument();
+  });
+
+  test('does not show total price for empty cart', () => {
+    render(<CardIcon price="0" />);
+    expect(screen.queryByText(/\$0/i)).not.toBeInTheDocument();
+  });
+});
