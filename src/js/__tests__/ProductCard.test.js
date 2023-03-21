ff653bd88962ef14e97029d84d45bdbc229efc3b
Ivan Greqov <idgreq@bk.ru>
2023-03-21 11:00:54 +0300

    Product card: add image, title, price

--- src/js/__tests__/ProductCard.test.js
+++ src/js/__tests__/ProductCard.test.js
@@ -3,8 +3,21 @@ import { render, screen } from '@testing-library/react';
 import ProductCard from '../components/ProductCard';
 
 describe('Product card', () => {
-  test('renders correctly', () => {
-    render(<ProductCard />);
-    expect(screen.getByText(/such product/i)).toBeInTheDocument();
+  test('has image, title and price', () => {
+    const product = {
+      img: {
+        src: 'product.png',
+        alt: 'product alt text',
+      },
+      title: 'product title',
+      price: 20,
+    };
+
+    render(<ProductCard product={product} />);
+
+    // TODO: not sure if src is a good attr to test
+    expect(screen.getByRole('img')).toHaveAttribute('src', product.img.src);
+    expect(screen.getByRole('heading')).toHaveTextContent(product.title);
+    expect(screen.getByText(/\$20/i)).toBeInTheDocument();
   });
 });
--- src/js/components/ProductCard.js
+++ src/js/components/ProductCard.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function ProductCard() {
-  return <div>such product!</div>;
+export default function ProductCard({ product }) {
+  const { img, title, price } = product;
+
+  return (
+    <div className="p-3 border rounded">
+      <img src={img.src} alt={img.alt} />
+      <h3>{title}</h3>
+      <div>price: {`$${price}`}</div>
+      <div>counter here</div>
+      <button type="button">Add to cart</button>
+    </div>
+  );
 }
